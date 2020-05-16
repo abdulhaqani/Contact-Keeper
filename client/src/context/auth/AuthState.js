50e8c948b1c9c34ee1406c7326cbe93658a60cca
Abdul Haqani <ahaqa011@uottawa.ca>
2020-05-15 22:53:14 -0400

    commit

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -66,8 +66,28 @@ const AuthState = (props) => {
   };
 
   // login user
-  const loginUser = () => {
-    console.log('login');
+  const loginUser = async (formData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
   };
 
   // logout
--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -30,6 +30,25 @@ export default (state, action) => {
         user: null,
         error: action.payload,
       };
+    case LOGIN_SUCCESS:
+      localStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        loading: false,
+        error: null,
+      };
+    case LOGIN_FAIL:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+        error: action.payload,
+      };
     case CLEAR_ERRORS:
       return {
         ...state,
