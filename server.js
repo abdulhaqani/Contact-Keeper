3c56436ab65ce5ca38e2ed2d511b5400d41b1003
Abdul Haqani <ahaqa011@uottawa.ca>
2020-05-17 03:06:12 -0400

    final commit

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ connectDB();
 // Init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'welcome to the Contact Keeper api' })
-);
-
 // Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
